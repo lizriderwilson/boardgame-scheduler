e6b064b9daed4e1cd6da40887f9869ebfda6094b
Liz Wilson <lizriderwilson@gmail.com>
2021-09-26 12:13:56 -0400

    Add Navbar placeholder and change tailwind default yellow from amber to yellow

---
+++ frontend/src/containers/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div className="h-24 bg-yellow-900">
+        <h1>Hi</h1>
+      </div>
+    )
+  }
+}
+
+export default Navbar
