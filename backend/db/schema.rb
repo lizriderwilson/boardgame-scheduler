d76f90a6fba8f5a96ee7a0ada7548b58c11066c1
Liz Wilson <lizriderwilson@gmail.com>
2021-09-30 09:29:59 -0400

    Generate Game model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_173405) do
+ActiveRecord::Schema.define(version: 2021_09_30_132539) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.string "number_of_players"
+    t.string "time"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
